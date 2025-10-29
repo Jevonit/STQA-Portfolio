9c3fdae84823128c0535d1f382c8af517504a730
jevonit <jevonit252@gmail.com>
2025-10-29 23:50:45 +0800

    Rollback

--- js/nav.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    const nav = document.querySelector('nav ul');
-    nav.classList.toggle('show');
-    
-    const menuBtn = document.querySelector('.menu-btn');
-    menuBtn.classList.toggle('active');
-}
